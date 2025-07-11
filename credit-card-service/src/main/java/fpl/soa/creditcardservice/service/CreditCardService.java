d86c0ed54d4d677e5e51e630f99db81616212250
ImAdSamadi <imad.samadi@etu.uae.ac.ma>
2025-07-11 17:51:18 +0100

    Removing Credit-Card Service

--- credit-card-service/src/main/java/fpl/soa/creditcardservice/service/CreditCardService.java
+++
@@ -1,9 +0,0 @@
-package fpl.soa.creditcardservice.service;
-
-import fpl.soa.creditcardservice.entities.CreditCard;
-
-
-public interface CreditCardService {
-    CreditCard getCreditCardById(String customerId);
-    CreditCard createCreditCard(CreditCard creditCard);
-}
